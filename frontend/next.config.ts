b50068b94270b8ca1fd0540e81e26694a346b747
harrietkallberg <120216648+harrietkallberg@users.noreply.github.com>
2026-05-03 15:47:13 +0100

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const backendDomain =
-  process.env.BACKEND_DOMAIN ?? process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+  process.env.BACKEND_DOMAIN || process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
 
 const nextConfig: NextConfig = {
   output: "standalone",
