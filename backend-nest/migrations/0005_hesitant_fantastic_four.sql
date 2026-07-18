5215bdb3cb87d3f833b7e20683324968ef0a1967
eddbb <edvinbl@kth.se>
2026-07-18 21:04:49 +0200

    fix: migrations

--- backend-nest/migrations/0005_hesitant_fantastic_four.sql
+++ backend-nest/migrations/0005_hesitant_fantastic_four.sql
@@ -9,6 +9,6 @@ CREATE TABLE "user_courses" (
 --> statement-breakpoint
 ALTER TABLE "user_courses" ADD CONSTRAINT "user_courses_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_courses" ADD CONSTRAINT "user_courses_course_code_courses_code_fk" FOREIGN KEY ("course_code") REFERENCES "public"."courses"("code") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "courses_embedding_idx" ON "courses" USING hnsw ("embedding" vector_cosine_ops);--> statement-breakpoint
-CREATE INDEX "courses_name_trgm_idx" ON "courses" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
-CREATE INDEX "courses_code_trgm_idx" ON "courses" USING gin ("code" gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS "courses_embedding_idx" ON "courses" USING hnsw ("embedding" vector_cosine_ops);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "courses_name_trgm_idx" ON "courses" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "courses_code_trgm_idx" ON "courses" USING gin ("code" gin_trgm_ops);
